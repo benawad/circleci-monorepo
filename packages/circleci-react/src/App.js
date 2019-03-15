c51f9b85d7ae30d65a62a1029bdb6c4eed37cebe
Ben Awad <benawad97@gmail.com>
2019-03-14 19:54:19 -0500

    lc

--- packages/circleci-react/src/App.js
+++ packages/circleci-react/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
 class App extends Component {
   render() {
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn react
           </a>
         </header>
       </div>
