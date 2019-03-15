118557486cb3422d7344f6993242231c8840ebf7
Ben Awad <benawad97@gmail.com>
2019-03-14 19:05:04 -0500

    timber3

--- packages/circleci-node/src/index.ts
+++ packages/circleci-node/src/index.ts
@@ -2,11 +2,11 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 import { User } from "./entity/User";
 
-createConnection().then(async connection => {
-
+createConnection()
+  .then(async connection => {
     console.log("Inserting a new user into the database...");
     const user = new User();
-    user.firstName = "Timber2";
+    user.firstName = "Timber3";
     user.lastName = "Saw";
     user.age = 25;
     await connection.manager.save(user);
@@ -17,5 +17,5 @@ createConnection().then(async connection => {
     console.log("Loaded users: ", users);
 
     console.log("Here you can setup and run express/koa/any other framework.");
-
-}).catch(error => console.log(error));
+  })
+  .catch(error => console.log(error));
